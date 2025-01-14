11e3c54def6d53c4c5749fd0f359a230f8aedb95
n1ghtmare <82060888+n1ghtmare-dev@users.noreply.github.com>
2025-01-14 20:27:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,72 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
-const popupContainer = document.getElementById("popup-container");
-const popup = document.getElementById("popup");
+class TaskManager {
+  constructor(){
+    // переменные
+    this.inputBox = document.getElementById("input-box");
+    this.listContainer = document.getElementById("list-container");
+    this.popupContainer = document.getElementById("popup-container");
+    this.popup = document.getElementById("popup");
+    this.btnAdd = document.getElementById("btn-add")
 
-function addTask(){
-  if(inputBox.value === '') {
-    alert("You must write something!");
+    // отслеживаем всякие штуки
+    this.listContainer.addEventListener("click", (e) => {
+      if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        this.saveData();
+      } else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        this.saveData();
+      }
+    }, false);
+
+    this.btnAdd.addEventListener("click", () => {
+      console.log('sdfsdf')
+      this.addTask();
+    })
+
+  }
+
+  addTask(){
+  if(this.inputBox.value === '') {
+      alert("You must write something!");
   } else {
-    let li = document.createElement("li");
-    let liP = document.createElement("p");
-    liP.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    let liBtn = document.createElement("button");
-    liBtn.innerHTML = "Посмотреть";
-    let liId = document.getElementsByTagName("li").length; 
-    liBtn.setAttribute("onclick", "infoToggle("+liId+")");
-    li.setAttribute("id", "li-"+liId);
-    li.appendChild(liP);
-    li.appendChild(span);
-    li.appendChild(liBtn);
+      let li = document.createElement("li");
+      let liP = document.createElement("p");
+      liP.innerHTML = this.inputBox.value;
+      this.listContainer.appendChild(li);
+      let span = document.createElement("span");
+      span.innerHTML = "\u00d7";
+      let liBtn = document.createElement("button");
+      liBtn.innerHTML = "Посмотреть";
+      let liId = document.getElementsByTagName("li").length; 
+      liBtn.setAttribute("onclick", "infoToggle("+liId+");");
+      li.setAttribute("id", "li-"+liId);
+      li.appendChild(liP);
+      li.appendChild(span);
+      li.appendChild(liBtn);
   }
-  inputBox.value = "";
-  saveData();
-}
-
-listContainer.addEventListener("click", function(e){
-  if(e.target.tagName === "LI"){
-    e.target.classList.toggle("checked");
-    saveData();
-  } else if(e.target.tagName === "SPAN") {
-    e.target.parentElement.remove();
-    saveData();
+  this.inputBox.value = "";
+  this.saveData();
   }
-}, false);
 
-function saveData(){
-  localStorage.setItem("data", listContainer.innerHTML);
+  saveData(){
+    localStorage.setItem("data", this.listContainer.innerHTML);
+  }
+  
+  showTask(){
+    this.listContainer.innerHTML = localStorage.getItem("data");
+  }
+
 }
 
-function showTask(){
-  listContainer.innerHTML = localStorage.getItem("data");
-}
+
+const taskManager = new TaskManager();
+taskManager.showTask();
+
 
 function infoToggle(n){
-  popupContainer.classList.toggle("active");
+  taskManager.popupContainer.classList.toggle("active");
   if(n != 0) {
     let li = document.getElementById("li-"+n);
-    popup.innerHTML = li.children[0].textContent;
+    taskManager.popup.innerHTML = li.children[0].textContent;
   }
 }
-
-function openPopup(){
-
-}
-
-showTask();
